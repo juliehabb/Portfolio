e5a8480d3834f43165522498b8dc5bc67dea0f91
juliehabb <julie.habbestad@hotmail.com>
2024-08-10 16:43:12 +0200

    Added filter function

---
+++ js/filter.mjs
@@ -0,0 +1,24 @@
+//select all filter buttons and cards
+const filterButtons = document.querySelectorAll(".filter-buttons div");
+const filterableCards = document.querySelectorAll(".card-container .vertical-card");
+
+const card = document.querySelector(".vertical-card");
+
+//define filterCards function
+const filterCards = e => {
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+
+    filterableCards.forEach(card => {
+        card.classList.add("hide");
+
+        if(card.dataset.name === e.target.dataset.name ){
+            card.classList.remove("hide");
+
+        }
+    });
+};
+
+
+//add click event listener to each filter button
+filterButtons.forEach(div => div.addEventListener("click", filterCards));
